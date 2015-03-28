5b21e0f636eb5a59d9a6544d8a019ab8dc42710e
Trevor John <trevor@john@tj>
2015-03-28 00:58:50 -0400

    add MLB#name_brief

--- app/models/mlb.rb
+++ app/models/mlb.rb
@@ -1,2 +1,15 @@
 class MLB < Player
+  def name_brief
+    [first_inital, last_initial].compact.join(' ')
+  end
+
+  private
+
+  def first_inital
+    first_name[0] unless first_name.blank?
+  end
+
+  def last_initial
+    last_name[0] unless last_name.blank?
+  end
 end
--- spec/models/mlb_spec.rb
+++ spec/models/mlb_spec.rb
@@ -1,4 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe MLB, type: :model do
+  describe '#name_brief' do
+    subject { @player.name_brief }
+
+    context 'with first and last names' do
+      before do
+        @player = MLB.new first_name: 'Golden', last_name: 'Saltini'
+      end
+
+      it { is_expected.to eq 'G S' }
+    end
+
+    context 'with only first name' do
+      before do
+        @player = MLB.new first_name: 'Golden'
+      end
+
+      it { is_expected.to eq 'G' }
+    end
+
+    context 'with only last name' do
+      before do
+        @player = MLB.new last_name: 'Saltini'
+      end
+
+      it { is_expected.to eq 'S' }
+    end
+
+    context 'with neither first nor last name' do
+      before do
+        @player = MLB.new
+      end
+
+      it { is_expected.to be_blank }
+    end
+  end
 end
