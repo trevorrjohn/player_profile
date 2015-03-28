c9b1475e13ab6b23ba30826e697e56337972e56d
Trevor John <trevor@john@tj>
2015-03-28 01:00:59 -0400

    add NBA#name_brief

--- app/models/nba.rb
+++ app/models/nba.rb
@@ -1,3 +1,11 @@
 class NBA < Player
+  def name_brief
+    [first_name, last_initial].compact.join ' '
+  end
 
+  private
+
+  def last_initial
+    last_name[0] unless last_name.blank?
+  end
 end
