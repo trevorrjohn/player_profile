4d72174a8fc431fbb2c075bb4eea645896c6ac1a
Trevor John <trevor@john@tj>
2015-03-28 01:04:05 -0400

    add NFL#name_brief

--- app/models/nfl.rb
+++ app/models/nfl.rb
@@ -1,3 +1,11 @@
 class NFL < Player
+  def name_brief
+    [first_initial, last_name].compact.join ' '
+  end
 
+  private
+
+  def first_initial
+    first_name[0] unless first_name.blank?
+  end
 end
