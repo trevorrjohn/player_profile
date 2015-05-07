e2eebc1be5f8464b8650aa1ccfd6b2c8fa47cacc
Trevor John <trevor@john.tj>
2015-05-07 15:36:16 -0400

    clean up documentation

--- spec/acceptance/players_spec.rb
+++ spec/acceptance/players_spec.rb
@@ -10,23 +10,12 @@ resource 'Players' do
       @mlbers = FactoryGirl.create_list(:player, 3, :mlb)
     end
 
-    example 'it responds with success' do
-      do_request
-
-      expect(status).to eq 200
-    end
-
-    example 'it returns all users' do
-      do_request
-
-      expect(json_response['players'].size).to eq 12
-    end
-
-    example 'it serializes the player object' do
+    example 'Get all players' do
       do_request
 
       player = json_response['players'][0]
-
+      expect(status).to eq 200
+      expect(json_response['players'].size).to eq 12
       expect(player.keys).to match_array(%w{
                                          id
                                          first_name
@@ -40,20 +29,17 @@ resource 'Players' do
   end
 
   get 'api/v1/players/:id' do
+    parameter :id, 'Player id'
+
     before do
       @player = FactoryGirl.create :player, :nhl, age: 20, position: 'C'
       FactoryGirl.create :player, :nhl, age: 30, position: 'C'
     end
 
-    example 'it responds with success' do
+    example 'Get specific player' do
       do_request id: @player.public_id
 
       expect(status).to eq 200
-    end
-
-    example 'it responds with a player json' do
-      do_request id: @player.public_id
-
       expect(json_response['player']).to eq(
         'id' => @player.public_id,
         'first_name' => @player.first_name,
