f8f94642c5ee8805a0160267bfc5d15a1bbc5f91
Trevor John <trevor@john.tj>
2017-08-23 15:47:23 -0400

    only allow you to fetch players for each league

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -1,10 +1,12 @@
 module Api
   module V1
     class PlayersController < ApplicationController
+    rescue_from ActionController::ParameterMissing, with: :render_invalid_param
       def index
-        players_json = Rails.cache.fetch('players_json') do
+        players_json = Rails.cache.fetch("v1_#{type_param.downcase}_players_json") do
+          players = Player.where('lower(type) = ?', type_param.downcase)
           render_to_string json: ActiveModelSerializers::SerializableResource.new(
-            Player.all, root: :players).as_json
+            players, root: :players).as_json
         end
         render json: players_json
       end
@@ -13,6 +15,16 @@ module Api
         player = Player.find_by_public_id params[:id]
         render json: ActiveModelSerializers::SerializableResource.new(
           player, root: :player).as_json
+      end
+
+      private
+
+      def render_invalid_param
+        render json: { error: 'Invalid league parameter' }, status: 422
+      end
+
+      def type_param
+        params.require(:league)
       end
     end
   end
--- spec/acceptance/players_spec.rb
+++ spec/acceptance/players_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 require 'rspec_api_documentation/dsl'
 
 resource 'Players' do
+  parameter :league, 'league name eg. [NHL, NFL, MLB, NHL]', required: true
   get '/api/v1/players' do
     before do
       @nhlers = FactoryGirl.create_list(:player, 3, :nhl)
@@ -10,12 +11,18 @@ resource 'Players' do
       @mlbers = FactoryGirl.create_list(:player, 3, :mlb)
     end
 
-    example 'Get all players' do
+    example 'invalid parameter' do
       do_request
+      expect(status).to eq 422
+      expect(json_response).to eq('error' => 'Invalid league parameter')
+    end
+
+    example 'Get NBA players' do
+      do_request league: 'NBA'
 
       player = json_response['players'][0]
       expect(status).to eq 200
-      expect(json_response['players'].size).to eq 12
+      expect(json_response['players'].size).to eq 3
       expect(player.keys).to match_array(%w{
                                          id
                                          first_name
@@ -28,6 +35,70 @@ resource 'Players' do
                                          photo_url
                                          name_brief
                                          })
+    end
+
+    example 'Get NHL players' do
+      do_request league: 'NHL'
+
+      player = json_response['players'][0]
+      expect(status).to eq 200
+      expect(json_response['players'].size).to eq 3
+      expect(player.keys).to match_array(%w{
+                                         id
+                                         first_name
+                                         last_name
+                                         average_position_age_diff
+                                         age
+                                         position
+                                         league
+                                         team
+                                         photo_url
+                                         name_brief
+                                         })
+    end
+
+    example 'Get NFL players' do
+      do_request league: 'NFL'
+
+      player = json_response['players'][0]
+      expect(status).to eq 200
+      expect(json_response['players'].size).to eq 3
+      expect(json_response['players'].map{ |x| x['league'] }.uniq)
+        .to eq ['NFL']
+      expect(player.keys).to match_array(
+        %w[
+           id
+           first_name
+           last_name
+           average_position_age_diff
+           age
+           position
+           league
+           team
+           photo_url
+           name_brief
+      ])
+    end
+
+    example 'Get MLB players' do
+      do_request league: 'MLB'
+
+      player = json_response['players'][0]
+      expect(status).to eq 200
+      expect(json_response['players'].size).to eq 3
+      expect(player.keys).to match_array(
+        %w[
+           id
+           first_name
+           last_name
+           average_position_age_diff
+           age
+           position
+           league
+           team
+           photo_url
+           name_brief
+      ])
     end
   end
 
