a9ffa1f4ef3dd91648cccc63d9035a22908a5538
Trevor John <trevor@john.tj>
2017-08-24 12:00:37 -0400

    update migrations to 5.1

--- db/migrate/20170823160836_add_team_and_photo_to_players.rb
+++ db/migrate/20170823160836_add_team_and_photo_to_players.rb
@@ -1,4 +1,4 @@
-class AddTeamAndPhotoToPlayers < ActiveRecord::Migration
+class AddTeamAndPhotoToPlayers < ActiveRecord::Migration[5.1]
   def change
     add_column :players, :team, :string
     add_column :players, :photo_url, :string
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -17,16 +16,16 @@ ActiveRecord::Schema.define(version: 20170823160836) do
   enable_extension "plpgsql"
 
   create_table "players", force: :cascade do |t|
-    t.string   "public_id"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "position"
-    t.integer  "age"
-    t.string   "type",       null: false
+    t.string "public_id"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "position"
+    t.integer "age"
+    t.string "type", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "team"
-    t.string   "photo_url"
+    t.string "team"
+    t.string "photo_url"
   end
 
 end
